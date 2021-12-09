5f6bf51f86129329f6988d0578ee2945dff1dfe9
Suguram Krishna <suguramkrishna@gmail.com>
2021-12-09 15:47:20 +0530

    js validation updated

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,39 +1,8 @@
-// function submitHandler(){
-//     let email = document.getElementById('emailInput').value;
-//     let password = document.getElementById('password').value;
-
-//     localStorage.setItem('email',email);
-//     localStorage.setItem('password',password);
 
 
 
-// }
-
-let login=[];
-
-// function pageOnLoad(){
-//     const user = JSON.parse(localStorage.getItem('details'))
-//     if(user){
-//         user=login;
-//     }
-// }
 
 
-// function submitHandler(){
-//     event.preventDefault();                                                                                                                                     
-//     const email = document.getElementById("email").value;
-//     const password = document.getElementById("password").value;
-
-//     const details = {
-//         "email": email,
-//         "password": password,
-//     }
-
+function submitHandler(){
     
-    
-//     login.push(details)
-//     localStorage.setItem("details", JSON.stringify(login))
-// }
-
-
-
+}
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,77 +1,73 @@
-let signup=[];
+let signup = [];
 
-function pageOnLoad(){
-    const user = JSON.parse(localStorage.getItem('signUpDetails'))
-    if(user){
-        user=signup;
-    }
+function pageOnLoad() {
+  const user = JSON.parse(localStorage.getItem("details"));
+  if (user != null) {
+    signup = user;
+  }
 }
 
+function submitHandler() {
+  event.preventDefault();
+  const name = document.getElementById("userName").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
 
-function submitHandler(){
-    event.preventDefault();
-    const name = document.getElementById("userName").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
+  const details = {
+    "name": name,
+    "email": email,
+    "password": password,
+    "confirmPassword": confirmPassword,
+  };
 
-    const details = {
-        "name": name,
-        "email": email,
-        "password": password,
-        "confirmPassword": confirmPassword
-    }
+  const emailValidation = mailCheck(email);
+  if (emailValidation) {
+    alert("Mail Id already Exsists");
+    return;
+  }
 
-   const emailValidation = mailCheck(email);
-   if(emailValidation){
-       alert("Mail Id already Exsists")
-       return;
-   }
-
-
-    const passMatch = check();
-    if(passMatch){
-        signup.push(details)
-        localStorage.setItem("details", JSON.stringify(signup));
-        window.location.href = "../pages/pvrlogin.html"
-    } else{
-        alert("Password Is not matching")
-    }   
-    
+  const passMatch = check();
+  if (passMatch) {
+    signup.push(details);
+    localStorage.setItem("details", JSON.stringify(signup));
+    window.location.href = "../pages/pvrlogin.html";
+  } else {
+    alert("Password Is not matching");
+  }
 }
-function check(){
-    event.preventDefault()
+function check() {
+  event.preventDefault();
 
-    let a = document.getElementById("password").value;
-    let b =  document.getElementById("confirmPassword").value;
-    if(a==b){
-        // console.log("Matching");
-        return true;
-    }
-    else{
+  let a = document.getElementById("password").value;
+  let b = document.getElementById("confirmPassword").value;
+  if (a == b) {
+    return true;
+  } else {
     return false;
-    }   
+  }
 }
 
-function mailCheck(mailInAct){
-    event.preventDefault()
+function mailCheck(mailInAct) {
+  event.preventDefault();
+  const userEmail = JSON.parse(localStorage.getItem("details"));
+  let alreadyExsists = false;
 
-    const userEmail = JSON.parse(localStorage.getItem("details"));
-
-    let alreadyExsists = false;
-
-    for(i=0; i<userEmail.length; i++){
-        const user = userEmail[i];
-        const email = user.email;
-
-        if(mailInAct == email){
-            alreadyExsists = true;
-            break;
-        }
+    if(userEmail != null){
+        for (i = 0; i < userEmail.length; i++) {
+            const user = userEmail[i];
+            const email = user.email;
+        
+            if (mailInAct == email) {
+              alreadyExsists = true;
+              break;
+            }
+          }
     }
 
-    return alreadyExsists;
+
+
+  return alreadyExsists;
 }
 
-       
-pageOnLoad()
+pageOnLoad();
