671a9dbb874fc3fce15c496be7cb14af90e47990
Suguram Krishna <suguramkrishna@gmail.com>
2021-12-17 15:42:30 +0530

    modified the js

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,7 +11,31 @@ function pageOnLoad() {
 }
 
 
-function submitHandler(userMailId, userPassword) {
+function submitHandler() {
+  event.preventDefault();
+
+
+  const userMailId = document.getElementById("email").value;
+  const passWord = document.getElementById("password").value;
+
+
+  let isDetailExist = isUserDetailExist(userMailId, passWord);
+  if (isDetailExist !== true) {
+      document.getElementById("invalidError").innerText = "Invalid login credentials";
+      return null;
+
+  }
+  else {
+
+      window.location.href = "./../../index.html";
+      localStorage.setItem("LoggedInUser", userMailId);
+  }
+
+
+}
+
+
+function isUserDetailExist(userMailId, userPassword) {
     event.preventDefault();
     let isExist = false;
     const lengthOfArray = userDetails.length;
