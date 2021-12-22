d1c7a1e4b99e8f71ad30903b2e00fcafe649cd87
Suguram Krishna <suguramkrishna@gmail.com>
2021-12-22 16:22:38 +0530

    fixed the bug

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,7 @@ console.log(userDetails);
 
 function pageOnLoad() {
   const loginUser = JSON.parse(localStorage.getItem("details"));
-  if (loginUser  !== null) {
+  if (loginUser !== null) {
     userDetails = loginUser;
   }
   console.log(loginUser);
@@ -23,14 +23,14 @@ function submitHandler(event) {
 
   let isDetailExist = isUserDetailExist(userMailId, userPass);
   if (isDetailExist !== true) {
-      document.getElementById("invalidError").innerText = "Invalid login credentials";
-      return null;
+    document.getElementById("invalidError").innerText = "Invalid login credentials";
+    return null;
 
   }
   else {
 
-      window.location.href = "./../../index.html";
-      localStorage.setItem("LoggedInUser", userMailId);
+    window.location.href = "./../../index.html";
+    localStorage.setItem("LoggedInUser", userMailId);
   }
 
 
@@ -38,36 +38,35 @@ function submitHandler(event) {
 
 
 function isUserDetailExist(userMailId, userPassword) {
-    let isExist = false;
-    const lengthOfArray = userDetails.length;
+  let isExist = false;
+  const lengthOfArray = userDetails.length;
 
-    for (let i = 0; i < lengthOfArray; i++) {
-      const valuesInArray = userDetails[i];
-      const userEmail = valuesInArray.email;
-      console.log(userEmail);
-      const userPass = valuesInArray.password;
-      console.log(userPass);
-      if (userMailId == userEmail && userPassword == userPass) {
-          isExist = true;
-          break;
-      }
+  for (let i = 0; i < lengthOfArray; i++) {
+    const valuesInArray = userDetails[i];
+    const userEmail = valuesInArray.email;
+    console.log(userEmail);
+    const userPass = valuesInArray.password;
+    console.log(userPass);
+    if (userMailId == userEmail && userPassword == userPass) {
+      isExist = true;
+      break;
+    }
   }
   return isExist;
 }
 
 
-function showPassword(){
-  const checkbox = document.getElementById("passCheckbox");
-  if(checkbox.checked){
-    document.getElementById("password").type="text";
+function showPassword() {
+  const passwordCheckBox = document.getElementById("passCheckbox");
+  if (passwordCheckBox.type === "password") {
+    passwordCheckBox.type = "text";
   }
-  else{
-    document.getElementById("password").type="password";
+  else {
+    passwordCheckBox.type = "password";
   }
-}
 
 
-pageOnLoad()
+  pageOnLoad()
 
 
 
