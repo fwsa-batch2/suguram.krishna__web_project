8926387a31e64cb67b3bf882c74e9a8ae82a6090
Suguram Krishna <suguramkrishna@gmail.com>
2021-12-29 00:39:50 +0530

    Modified the pages

--- assets/js/movie_list.js
+++ assets/js/movie_list.js
@@ -2,14 +2,16 @@ let movie = "";
 let movieDetails = JSON.parse(localStorage.getItem("details"));
 for(let i of movieDetails){
     let nameOfMovie = i.movieName;
-    let nameOFHero = i.heroName;
+    let nameOfHero = i.heroName;
     let nameOfDirector = i.directorName;
+    let  descriptionOfMovie = i.movieDescription;
    
     movie += 
    "<div class='movieInfo' id='movieInfo'><p class='movieDetails'>Movie Name:<span class='movieNameSpan'>"+
    nameOfMovie+"</span></p><p class='movieDetails'>Hero Name:<span class='heroNameSpan'>"+
-        nameOFHero+"</span></p><p class='movieDetails'>Director Name:<span class='directorNameSpan'>"+
-        nameOfDirector+"</span></p></div>"
+        nameOfHero+"</span></p><p class='movieDetails'>Director Name:<span class='directorNameSpan'>"+
+        nameOfDirector+"</span></p> <p class='movieDetails'>Movie Description:<span class='descriptionSpan'>"+
+        descriptionOfMovie+"</span></p></div>"
 }
 
 let div = document.getElementById("movieListing");
